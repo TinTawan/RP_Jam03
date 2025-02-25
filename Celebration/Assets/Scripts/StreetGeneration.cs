2c8c9a48488da03775722905ce4ade27405bf2f9
Justin Ward <jw4566c@gre.ac.uk>
2025-02-25 20:41:27 +0000

    basic street generation with low chance of cross road

--- Celebration/Assets/Scripts/StreetGeneration.cs
+++ Celebration/Assets/Scripts/StreetGeneration.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class StreetGeneration : MonoBehaviour
 {
     [SerializeField] GameObject[] streetChunks; //0 = forward road, 1 = cross road
+    [SerializeField] Transform startPos;
+    [Tooltip("Number of chunks to spawn")] [SerializeField] int streetLength;
+    [Tooltip("Percentage chance for a Cross Roads chunk to spawn")] [SerializeField] [Range(1, 100)] int chanceForCR = 30;
 
-    [SerializeField] int streetLength;
 
     private void Start()
     {
         GenerateRandomStreet();
+
     }
 
     void GenerateRandomStreet()
     {
-        GameObject chunk = Instantiate(streetChunks[Random.Range(0, streetChunks.Length)])
+        for(int i = 0; i < streetLength; i++)
+        {
+            Vector3 pos = new(startPos.position.x, startPos.position.y, startPos.position.z + (i * 30));
+            /*GameObject chunk = */Instantiate(RandChunk(), pos, Quaternion.identity, transform);
+
+
+        }
+        
+    }
+
+    GameObject RandChunk()
+    {
+        //more chance for forward road than cross road
+        if(Random.Range(0,100) > chanceForCR)
+        {
+            return streetChunks[0];
+        }
+        else
+        {
+            return streetChunks[1];
+        }
+
     }
 }
