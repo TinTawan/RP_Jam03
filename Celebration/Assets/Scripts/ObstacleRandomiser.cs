1b7e18d1b8af1f0702a65b092c7c1216538d170a
Justin Ward <jw4566c@gre.ac.uk>
2025-02-25 22:09:48 +0000

    obstacle spawn pos and rot randomised
    randomises position and rotation by delta value

--- Celebration/Assets/Scripts/ObstacleRandomiser.cs
+++ Celebration/Assets/Scripts/ObstacleRandomiser.cs
@@ -7,6 +7,8 @@ public class ObstacleRandomiser : MonoBehaviour
     [SerializeField] Transform[] possibleSpawnLocations;
     [SerializeField] GameObject[] obstacles;
 
+    [SerializeField] float positionDelta = 1f, rotationDelta = 45f;
+
     int spawnCount;
 
     private void Start()
@@ -20,11 +22,27 @@ public class ObstacleRandomiser : MonoBehaviour
     {
         for (int i = 0; i < possibleSpawnLocations.Length; i++)
         {
-            Instantiate(obstacles[Random.Range(0,obstacles.Length)], possibleSpawnLocations[i].position, Quaternion.identity, possibleSpawnLocations[i]);
+            Instantiate(obstacles[Random.Range(0,obstacles.Length)], RandPos(possibleSpawnLocations[i].position, positionDelta), RandYRot(Quaternion.identity, rotationDelta), possibleSpawnLocations[i]);
 
         }
 
+    }
 
+    Vector3 RandPos(Vector3 midPoint, float delta)
+    {
+        float x = Random.Range(midPoint.x - delta, midPoint.x + delta);
+        float z = Random.Range(midPoint.z - delta, midPoint.z + delta);
+
+        return new(x, midPoint.y, z);
+    }
+
+    Quaternion RandYRot(Quaternion curRot, float delta)
+    {
+        Vector3 euler = curRot.eulerAngles;
+
+        float yRot = Random.Range(euler.y - delta, euler.y + delta);
+
+        return Quaternion.Euler(curRot.x, yRot, curRot.z);
     }
 
 
