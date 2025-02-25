6a51dd39bffd7d04c2eea74f43a65e61ab3e0c15
Justin Ward <jw4566c@gre.ac.uk>
2025-02-25 01:15:52 +0000

    UFO makes player float (kinda)
    doesnt work every time but oh well

---
+++ Celebration/Assets/Scripts/UFObstacle.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFObstacle : MonoBehaviour
+{
+
+    bool inside;
+    PlayerMovement pMove;
+    List<Rigidbody> playerRbs = new List<Rigidbody>();
+
+    [SerializeField] float floatForce = 10f, floatTime = 3f;
+
+    private void Update()
+    {
+        if (inside && pMove != null)
+        {
+            foreach(Rigidbody rb in playerRbs)
+            {
+                rb.AddForce(Vector3.up * floatForce, ForceMode.Acceleration);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            inside = true;
+
+            if(col.TryGetComponent(out PlayerMovement pm))
+            {
+                pMove = pm;
+            }
+
+            StartCoroutine(FloatPlayer());
+
+            playerRbs.Add(col.GetComponent<Rigidbody>());
+            
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            inside = false;
+
+        }
+
+        playerRbs.Clear();
+    }
+
+    IEnumerator FloatPlayer()
+    {
+        if(pMove != null)
+        {
+            pMove.SetPlayerRagdoll(true);
+            GetComponentInChildren<CapsuleCollider>().enabled = false;
+
+            yield return new WaitForSeconds(floatTime);
+
+            inside = false;
+            pMove.SetPlayerRagdoll(false);
+
+            yield return new WaitForSeconds(floatTime);
+
+            GetComponentInChildren<CapsuleCollider>().enabled = true;
+        }
+        
+    }
+}
